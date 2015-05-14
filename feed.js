02bb250d9decfc64fe832c7dbe55470a37329f59
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-05-14 19:21:20 +0530

    dsfsd

--- feed.js
+++ feed.js
@@ -3,24 +3,9 @@ var host=window.location.host;
 var url=window.location.href;
 
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://52.74.43.53/growth-heacker/feed_gh.php?key="+key+"&host="+host+"&url="+url, true);
-xhr.onload = function (e) {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      console.log(xhr.responseText);
-	  document.body.insertAdjacentHTML('afterbegin', xhr.responseText);
-    } else {
-      console.error(xhr.statusText);
-    }
-  }
-};
-xhr.onerror = function (e) {
-  console.error(xhr.statusText);
-};
-xhr.send(null);
-
-
+$(document).ready(function(){
+	$("body").load("http://52.74.43.53/growth-heacker/feed_gh.php?key="+key+"&host="+host+"&url="+url);
+});
 
 
 
