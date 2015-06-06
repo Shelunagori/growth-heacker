44489d8722e08131803d606739c8de6362caafa1
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-06-06 16:17:01 +0530

    sdfsdf

--- feed.js
+++ feed.js
@@ -1,10 +1,28 @@
-
-(function(a) {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);}(window, document))
-
 var xmlhttp;
 var host=window.location.host;
 var url=window.location.href;
 alert();
+
+
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    alert(xmlhttp.responseText);
+    }
+  }
+xmlhttp.open("GET","http://52.74.43.53/growth-heacker/feed_gh.php?key="+key+"&host="+host+"&url="+url",true);
+xmlhttp.send();
+
 
 $(document).ready(function(){
 	alert();
