7aad549107ba0871e1ed3667338316640bd86b7d
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-06-08 12:36:49 +0530

    sdff

--- feed.js
+++ feed.js
@@ -1,43 +1,31 @@
-var xmlhttp;
 var host=window.location.host;
 var url=window.location.href;
-alert(host);
-alert(url);
 
 
-var xobj;
-//modern browers
-if(window.XMLHttpRequest)
-{
-xobj=new XMLHttpRequest();
+
+function createCORSRequest(method, url){
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr){
+        // XHR has 'withCredentials' property only if it supports CORS
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){ // if IE use XDR
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
 }
-//for ie
-else if(window.ActiveXObject)
-{
-xobj=new ActiveXObject("Microsoft.XMLHTTP");
-}
-else
-{
-alert("Your broweser doesnot support ajax");
-}
-if(xobj)
-{
+
+
 var query="?key=" + key +"&host="+host+"&url="+url;
-
-xobj.open("GET","http://52.74.43.53/growth-heacker/feed_gh.php" +query,true);
-xobj.onreadystatechange=function()
-{
-	alert(xobj.readyState);
-	alert(xobj.status);
-if(xobj.readyState==4 && xobj.status==200)
-{	   
-document.body.innerHTML += xobj.responseText;
+var request = createCORSRequest( "get", "http://52.74.43.53/growth-heacker/feed_gh.php" +query );
+if ( request ){
+    // Define a callback function
+    request.onload = function(){};
+    // Send request
+    request.send();
 }
-}
-
-}
-xobj.send(null);
-
 
 
 
