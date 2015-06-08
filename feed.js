ac6eca96ea9880136e32c209c3edcff7c1d09235
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-06-08 18:42:40 +0530

    dsdf

--- feed.js
+++ feed.js
@@ -3,8 +3,13 @@ var url=window.location.href;
 $(document).ready(function(){
 	var query="?key=" + key +"&host="+host+"&url="+url;
     var url="http://52.74.43.53/growth-heacker/feed_gh.php" +query;
+	$.ajax({
+        crossOrigin: true,
+        url: url,
+		dataType: 'jsonp' 
+        success: function(data) {
+			$("body").append(data);
+        }
+    });
 	
-	$.get(url, function(data) {
-		alert(data);
-	});
 });
