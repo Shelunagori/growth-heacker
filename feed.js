3be7ff09f8dd87e565b280cfba991a158dc27c85
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-06-09 19:18:23 +0530

    asdasd

--- feed.js
+++ feed.js
@@ -3,12 +3,10 @@ $(document).ready(function(){
     var url="http://52.74.43.53/growth-heacker/feed_gh.php";
 		alert(url);
 	$.ajax({
-        
-		type: 'GET',
 		url: url,
         success: function(data) {
 			alert(data);
-			$("body").append(data.report_type);
+			$("body").append(data);
         }
     });
 	
